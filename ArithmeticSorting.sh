9b79ebfc9c57632f04be44c9a8c9fb981cbb1036
unknown <nandiniswaraj13@gmail.com>
2021-02-16 22:29:40 +0530

    compute c+a/b in ArithmeticSorting.sh

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -2,5 +2,9 @@
 read -p "Enter 1st Number " a
 read -p "Enter 2nd Number " b
 read -p "Enter 3rd Number " c
-result1=$(( a*b+c ))
-echo $result1
+result1=$(( a+b*c ))
+reuilt2=$(( a*b+c ))
+result3=$(( c+a/b ))
+echo "a+b*c : " $result1
+echo "a*b+c : " $result2
+echo "c+a/b : " $result3
